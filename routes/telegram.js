e0439ed6c9e49368f2a06d150d209f526d1e927f
Anton Rabota <anton.rabota@sibir.rt.ru>
2025-03-31 23:33:49 +0700

    commit

--- routes/telegram.js
+++ routes/telegram.js
@@ -1,24 +1,30 @@
 import express from 'express';
-import axios from 'axios';
 import dotenv from 'dotenv';
 
 dotenv.config(); // Загружаем переменные окружения из .env
 
 const router = express.Router();
-const TELEGRAM_BOT_TOKEN = process.env.REACT_APP_TELEGRAM_BOT_TOKEN;
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const TELEGRAM_API_URL = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}`;
 
 router.post('/sendMessage', async (req, res) => {
   try {
     const { chat_id, text, parse_mode } = req.body;
-    
-    const response = await axios.post(`${TELEGRAM_API_URL}/sendMessage`, {
+
+   const response = await fetch(`${TELEGRAM_API_URL}/sendMessage`,{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
       chat_id,
       text,
       parse_mode: parse_mode || 'MarkdownV2'
-    });
+   })
+  });
+   const data = await response.json();
 
-    res.json(response.data);
+    res.json(data);
   } catch (error) {
     console.error('Ошибка при отправке сообщения:', error);
     res.status(500).json({ error: 'Ошибка при отправке сообщения в Telegram' });
