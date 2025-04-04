795d502b85f3eb14d40256eb0724d9e741a73a82
Anton Rabota <anton.rabota@sibir.rt.ru>
2025-04-04 20:26:41 +0700

    отладка сервака_17

--- server.js
+++ server.js
@@ -52,19 +52,20 @@ app.use((req, res, next) => {
 
 // 7. Подключение к БД
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'taigsql',
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 6543,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: 'Gjkmpjdfntkm1bpNfqubycrjujGfhrf!',
+  //password: String(process.env.DB_PASSWORD),
+  port: process.env.DB_PORT,
+  //ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: false
 });
 
 console.log('Database connection parameters:', {
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: '********', // Не показывать реальный пароль
   port: process.env.DB_PORT
 });
 
