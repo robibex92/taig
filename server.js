0d1585dfe17b57da76c82967591be22402cc1313
Anton Rabota <anton.rabota@sibir.rt.ru>
2025-04-04 21:44:45 +0700

    отладка сервака_26

--- server.js
+++ server.js
@@ -51,15 +51,22 @@ app.use((req, res, next) => {
 });
 
 // 7. Подключение к БД
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD.trim(),
-  port: process.env.DB_PORT,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
-});
+//const pool = new Pool({
+ // user: process.env.DB_USER,
+  //host: process.env.DB_HOST,
+  //database: process.env.DB_NAME,
+  //password: process.env.DB_PASSWORD.trim(),
+  //port: process.env.DB_PORT,
+  //ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+//});
 
+const pool = new Pool({
+  user: 'backend_user',
+  host: 'localhost',
+  database: 'taigsql',
+  password: 'Gjkmpjdfntkm1bpNfqubycrjujGfhrf!',
+  port: 6543
+});
 pool.on('connect', () => console.log('New DB connection'));
 pool.on('error', err => console.error('DB error:', err));
 
@@ -86,7 +93,6 @@ app.get('/api/health', async (req, res) => {
 });
 // Простейший тестовый endpoint
 app.get('/api/test', (req, res) => {
-  app.get('/api/test', (req, res) => {
      res.json({ message: "Hello from backend!" });
    });
    
