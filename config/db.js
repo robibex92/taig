05215d8b003d46c6d15dbff199c528b572314c5b
Anton Rabota <anton.rabota@sibir.rt.ru>
2025-06-04 23:51:02 +0700

    db.js на локалку

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
-import pg from 'pg';
-import dotenv from 'dotenv';
+import pg from "pg";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -7,12 +7,12 @@ const { Pool } = pg;
 
 export const pool = new Pool({
   user: process.env.DB_USER,
-  host: '185.219.81.226', 
-  //host: process.env.DB_HOST,
+  //host: '185.219.81.226',
+  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password:process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
-pool.on('connect', () => console.log('New DB connection'));
-pool.on('error', err => console.error('DB error:', err));
+pool.on("connect", () => console.log("New DB connection"));
+pool.on("error", (err) => console.error("DB error:", err));
