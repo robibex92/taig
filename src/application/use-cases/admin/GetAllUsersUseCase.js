09c463356d2fa36460378125778d29944f3d9630
Anton Rabota <anton.rabota@sibir.rt.ru>
2025-10-14 23:36:12 +0700

    Правки по роутам для админской статистики2

--- src/application/use-cases/admin/GetAllUsersUseCase.js
+++ src/application/use-cases/admin/GetAllUsersUseCase.js
@@ -1,3 +1,5 @@
+import logger from "../../../infrastructure/logger/index.js";
+
 /**
  * Get All Users Use Case
  * Admin endpoint to retrieve all users
@@ -16,12 +18,29 @@ export class GetAllUsersUseCase {
 
     if (role) {
       filters.status = role;
+      logger.info("[GetAllUsersUseCase] Filtering by role", {
+        role,
+        status: filters.status,
+      });
     }
+
+    logger.info("[GetAllUsersUseCase] Executing with filters", {
+      limit,
+      offset,
+      search,
+      role,
+      filters,
+    });
 
     const users = await this.userRepository.findAll({
       limit,
       offset,
       ...filters,
+    });
+
+    logger.info("[GetAllUsersUseCase] Users found", {
+      count: users.length,
+      total: await this.userRepository.count(filters),
     });
 
     const total = await this.userRepository.count(filters);
